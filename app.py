1f1a484018dbf51a8f9a1dcd51cec92d4c956599
sofa1306 <127314528+sofa1306@users.noreply.github.com>
2023-03-08 15:46:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
-app = Flask(__name__)
+import PySimpleGUIWeb as sg
+
+app = Flask(__name__)    
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_world():    
+    layout = [[sg.Text('demo')],      
+                     [sg.Text("name"),sg.InputText()],
+                     [sg.Text("age"),sg.InputText()],
+                     [sg.Text("tall"),sg.InputText()],
+                     [sg.Submit(), sg.Cancel()]]      
+
+    window = sg.Window('Window Title', layout)    
+
+    event, values = window.read()    
+    window.close()
+
+    text_input = values[0], values[1], values[2]
+    sg.popup('Your info', text_input)
